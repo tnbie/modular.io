7abdcd8d5cbf548af082ab50af9d0cc410fa973a
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2023-12-06 23:35:16 -0300

    adjust resources on settings file

--- modulario/modulario/settings.py
+++ modulario/modulario/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'customers',
+    'notifications',
+    'orders',
+    'payments',
+    'products',
+    'tickets'
 ]
 
 MIDDLEWARE = [
